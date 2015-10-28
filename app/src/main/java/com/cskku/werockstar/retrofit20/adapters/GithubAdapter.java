35a62356e234877be408a0a4a7f93dc678b282cc
WeRockStar <vr_freestyle@outlook.com>
2015-10-29 02:27:41 +0700

    Create adapter

--- app/src/main/java/com/cskku/werockstar/retrofit20/adapters/GithubAdapter.java
+++ app/src/main/java/com/cskku/werockstar/retrofit20/adapters/GithubAdapter.java
@@ -1,7 +1,33 @@
 package com.cskku.werockstar.retrofit20.adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Kotchaphan on 29/10/2558.
  */
-public class GithubAdapter {
+public class GithubAdapter extends RecyclerView.Adapter<GithubAdapter.GithubViewHolder>{
+
+    @Override
+    public GithubViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(GithubViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class GithubViewHolder extends RecyclerView.ViewHolder{
+
+        public GithubViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
