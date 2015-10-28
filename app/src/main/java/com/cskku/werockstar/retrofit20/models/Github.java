8228c9f6594cec9fca5e98d14c0d3d4f40c3dc33
WeRockStar <vr_freestyle@outlook.com>
2015-10-29 02:38:00 +0700

    Pojo Github model

--- app/src/main/java/com/cskku/werockstar/retrofit20/models/Github.java
+++ app/src/main/java/com/cskku/werockstar/retrofit20/models/Github.java
@@ -1,11 +1,36 @@
 package com.cskku.werockstar.retrofit20.models;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Kotchaphan on 29/10/2558.
  */
 public class Github {
+    @SerializedName("name")
+    private String name;
 
-    private String username;
+    @SerializedName("company")
+    private String company;
 
+    @SerializedName("location")
+    private String location;
 
+    @SerializedName("avatar_url")
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
